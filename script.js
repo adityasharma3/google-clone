8fa4863c6e250e883cee81233d93df1de1a93ac6
Aditya Sharma <aditya.sharma9301@gmail.com>
2021-03-13 15:27:55 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("UP");
+
+function retrieve_query() {
+    var getQuery = document.getElementById("search-bar").value;
+}
+
+function google_query() {
+
+}
+
+function onClickSubmit() {
+    
+}
